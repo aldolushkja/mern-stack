27c3eb51cd312e5d1c4c4c636aa6bb35874d632c
alushkja <aldo.lushkja@consulenti.fastweb.it>
2020-04-23 21:26:53 +0200

    aggiunto il readme e il modulo bcrypt

---
+++ README.md
@@ -0,0 +1,34 @@
+## Instagram Clone
+
+### Environment
+1. NodeJS 3+
+2. Express JS
+3. MongoDB ( Cloud Atlas )
+4. ReactJS
+
+### Useful commands
+```shell
+npm init
+```
+Inizializza un progetto nodejs con il file package.json e cartella node_modules
+
+```shell
+npm install <nome_modulo>
+```
+Installa un modulo all'interno del node_modules e lo aggiugne a package.json come dependency
+
+```shell
+node app
+```
+Lancia l'applicazione con file app.js
+
+```shell
+nodemon app
+```
+Permette di fare hot-swap e hot-redeploy di applicativi in Node JS
+
+### Libraries
+1. npm install express
+2. npm install nodemon
+3. npm install mongoose
+4. npm install bcryptjs
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,17 +1,40 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+const User = mongoose.model("User")
+const bcrypt = require('bcryptjs')
 
-router.get("/", (req, res) => {
-    res.send("Hello from NodeJS");
-})
 
 router.post("/signup", (req, res) => {
     const { name, email, password } = req.body
     if (!email || !password || !name) {
         return res.status(422).json({ error: "please fill all fields" })
     }
-    res.json({
-        message: "successfully posted"
+    User.findOne({ email: email }).then((savedUser) => {
+        if (savedUser) {
+            return res.status(422).json({ error: "user already exists with that email" })
+        }
+
+        bcrypt.hash(password, 16).then(
+            (hashedpassword) => {
+                const user = new User({
+                    email: email,
+                    name: name,
+                    password: hashedpassword
+                })
+
+                user.save().then((user) => {
+                    res.status(201).json({ message: "saved successfully" })
+                }).catch(err => {
+                    console.log("Error saving user", err)
+                });
+            }
+        ).catch(err => {
+            console.log("Error encrypting current password", err)
+        })
+
+    }).catch(err => {
+        console.log("Error routing on /signup endpoint", err)
     })
 })
 
