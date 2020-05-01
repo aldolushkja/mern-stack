f985b71c41599ebf935b48cc1b92e0a31ca4bcad
alushkja <aldo.lushkja@consulenti.fastweb.it>
2020-05-01 21:37:18 +0200

    Home page UI

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <div className="home">
+
+            <div className="card home-card">
+                <h5>aldus</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"/>
+                </div>
+                <div className="card-content">
+                    <i className="material-icons" style={{color:"red"}}>favorite</i>
+                    <h6>Title</h6>
+                    <p>this is amazing post</p>
+                    <input type="text" placeholder="add a comment"/>
+                </div>
+            </div>
+
+        </div>
     )
 }
 
